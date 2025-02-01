93991ec171d2ee72bc2a245def1f9a8d198e7b36
NivethaAshok <nivethaashok@hotmail.com>
2025-02-01 12:31:04 -0500

    Initial commit: Adding ARM template

---
+++ Deploy command.ps1
@@ -0,0 +1,10 @@
+﻿$templateFile="C:\Azure Learning\ARM\azuredeploy.v1.2.json"
+$today=Get-Date -Format "MM-dd-yyyy"
+$deploymentName="addstorage-"+"$today"
+New-AzResourceGroupDeployment -Name $deploymentName -Templatefile $templateFile
+
+$templateFile="C:\Azure Learning\ARM\azuredeploy.v1.3.json"
+az deployment group create --name testdeploymentashok1 --resource-group company-1 --template-file $templateFile --parameters storageAccountType=Standard_LRS
+
+
+New-AzResourceGroupDeployment `-Name $deploymentName `-TemplateFile $templateFile `-storageName {your-unique-name}
